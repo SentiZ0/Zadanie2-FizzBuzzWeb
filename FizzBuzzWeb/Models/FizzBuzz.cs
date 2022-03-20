6242641f6d077359bb4d571cfe1b8f66e07151c1
jakkir <jakkir@LAPTOP-2GNB6A2H>
2022-03-20 23:13:52 +0100

    Add project files.

---
+++ FizzBuzzWeb/Models/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzzWeb.Models
+{
+    public class FizzBuzz
+    {
+        [Required, Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}."), Display(Name = "Twój szczęśliwy numerek")]
+        public int Number { get; set; }
+
+    }
+}
---
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using FizzBuzzWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public string Response { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnPost()
+        {
+            int number = FizzBuzz.Number;
+
+            if (!ModelState.IsValid)
+            {
+                Response = "Podana wartość jest nieprawidłowa";
+                return Page();
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                Response = "FizzBuzz";
+                return Page();
+            }
+            else if (number % 3 == 0)
+            {
+                Response = "Fizz";
+                return Page();
+            }
+            else if (number % 5 == 0)
+            {
+                Response = "Buzz";
+                return Page();
+            }
+            else
+            {
+                Response = $"Liczba: {number} nie spełnia kryteriów FizzBuzz";
+                return Page();
+            }
+            return Page();
+        }
+        public void OnGet()
+        {
+
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+    }
+}
